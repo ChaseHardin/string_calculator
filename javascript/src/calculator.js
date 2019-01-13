2cadf81a90abf1c8bdc4f155a2c22711f8ab702d
Chase Hardin <chase.hardin@scribbletechnology.com>
2019-01-13 14:37:48 -0600

    Should throw exception when negative number is provided.

--- javascript/src/calculator.js
+++ javascript/src/calculator.js
@@ -1,18 +1,28 @@
 NEW_LINE_DELIMITER = '\\n';
 COMMA_DELIMITER = ',';
 MIN_NUMBER = 0;
+MAX_NUMBER = 1000;
 
 function add(string) {
     if (!string)
         return MIN_NUMBER;
 
     const parsed = extractNumbers(string);
+    const negatives = filterBy(parsed, MIN_NUMBER);
+
+    if (negatives.length > 0)
+        return `Negatives not allowed: [${negatives}]`;
 
     return parsed.reduce((acc, value) => acc + value, MIN_NUMBER);
 }
 
 function extractNumbers(string) {
-    return sanitize(string).map(item => parseInt(item));
+    const parsed = sanitize(string).map(item => parseInt(item));
+    return filterBy(parsed, MAX_NUMBER);
+}
+
+function filterBy(parsed, value) {
+    return parsed.filter(item => item < value);
 }
 
 function sanitize(string) {
--- javascript/test/caclulator.spec.js
+++ javascript/test/caclulator.spec.js
@@ -22,6 +22,14 @@ describe('CalculatorTests', () => {
         assertAdd('1\\n2\\n3\\n4,5', 15);
     });
 
+    it('should exclude numbers greater than one thousand', () => {
+       assertAdd('1,2,1001', 3);
+    });
+
+    it('should throw an exception when provided with negative numbers', () => {
+        assertAdd('-1,2,3,4,-5', 'Negatives not allowed: [-1,-5]')
+    });
+
     function assertAdd(arguments, expected) {
         return expect(add(arguments)).to.eql(expected)
     }
