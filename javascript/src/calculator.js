65d865e4fc8a9ba54fcabf1cb4e3ab4845defdb3
Chase Hardin <chase.hardin@scribbletechnology.com>
2019-01-13 14:21:47 -0600

    Should return sum when given new ine and comma delimiter

--- javascript/src/calculator.js
+++ javascript/src/calculator.js
@@ -1,18 +1,24 @@
+NEW_LINE_DELIMITER = '\\n';
+COMMA_DELIMITER = ',';
+MIN_NUMBER = 0;
+
 function add(string) {
     if (!string)
-        return 0;
+        return MIN_NUMBER;
 
     const parsed = extractNumbers(string);
 
-    return parsed.reduce((acc, value) => acc + value, 0);
+    return parsed.reduce((acc, value) => acc + value, MIN_NUMBER);
 }
 
 function extractNumbers(string) {
-   return sanitize(string).map(item => parseInt(item));
+    return sanitize(string).map(item => parseInt(item));
 }
 
 function sanitize(string) {
-    return string.split(',');
+    return string.split(NEW_LINE_DELIMITER)
+        .join(COMMA_DELIMITER)
+        .split(COMMA_DELIMITER);
 }
 
 module.exports = {
--- javascript/test/caclulator.spec.js
+++ javascript/test/caclulator.spec.js
@@ -3,14 +3,26 @@ const {add} = require('../src/calculator');
 
 describe('CalculatorTests', () => {
     it('should return zero when given an empty string', () => {
-        expect(add('')).to.eql(0)
+        assertAdd('', 0);
     });
 
     it('should add return number when given only one', () => {
-       expect(add('1')).to.eql(1);
+        assertAdd('1', 1);
     });
 
-    it('should return sum when given comma separated string', () => {
-       expect(add('1,2,3,4,5')).to.eql(15);
+    it('should return sum when given comma delimiter', () => {
+        assertAdd('1,2,3,4,5', 15);
     });
+
+    it('should return sum when given a new line delimiter in string', () => {
+       assertAdd('1\\n2\\n3\\n4\\n5', 15);
+    });
+
+    it('should return sum when given new line and comma delimiter', () => {
+        assertAdd('1\\n2\\n3\\n4,5', 15);
+    });
+
+    function assertAdd(arguments, expected) {
+        return expect(add(arguments)).to.eql(expected)
+    }
 });
